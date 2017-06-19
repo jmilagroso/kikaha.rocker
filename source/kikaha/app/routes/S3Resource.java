17bedf9fd7096d8aac7310803062a06d68c53f0c
Jay Milagroso <j.milagroso@gmail.com>
2017-06-19 09:37:12 +0800

    Major update.

--- source/kikaha/app/routes/S3Resource.java
+++ source/kikaha/app/routes/S3Resource.java
@@ -1,58 +1,93 @@
 package kikaha.app.routes;
 
-import com.fizzed.rocker.Rocker;
+import io.netty.handler.codec.http.HttpHeaders;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 import kikaha.app.services.S3;
 import kikaha.core.modules.http.WebResource;
-import org.asynchttpclient.*;
+import org.asynchttpclient.AsyncCompletionHandler;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.BoundRequestBuilder;
+import org.asynchttpclient.HttpResponseBodyPart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xnio.channels.StreamSinkChannel;
+
 import javax.inject.Singleton;
 import javax.ws.rs.core.Response;
-import java.nio.charset.Charset;
+
+import static org.asynchttpclient.Dsl.asyncHttpClient;
+import static org.asynchttpclient.Dsl.config;
 
 @Singleton
 @WebResource( path="/s3", method="GET" )
 public class S3Resource implements HttpHandler {
 
-    Logger logger = LoggerFactory.getLogger(UndertowResource.class);
+    private final static Logger logger = LoggerFactory.getLogger(S3Resource.class);
+    private final static AsyncHttpClient httpClient = asyncHttpClient(config()
+            .setMaxConnections(2000)
+            .setMaxConnectionsPerHost(2000)
+            .setPooledConnectionIdleTimeout(100)
+            .setConnectionTtl(500)
+    );
 
     @Override
     public void handleRequest(final HttpServerExchange exchange) throws Exception {
-        try {
-            // Async http client instance.
-            AsyncHttpClient asyncHttpClient = new DefaultAsyncHttpClient();
+        // Please consider run blocking operations on Undertow's Worker Threads, instead of Undertow's IO Thread.
+        // Even under mid-level load on your server, you could face some IO issues if you don't do it.
+        // Using Undertow's API, one should always rely on the following pattern:
+        if ( !exchange.isInIoThread() ){
+            exchange.dispatch( this );
+            return;
+        }
 
-            // S3 AWS request instance.
+        try {
             S3.AwsS3Request awsS3Request = S3.get("README.md");
 
-            // Bount request builder (So we can inject HTTP request headers as needed)
-            BoundRequestBuilder boundRequestBuilder = asyncHttpClient.prepareGet(S3.getRequestURL());
+            //if content length is known (or will be known later... set a transfer encoding)
+            exchange.getResponseHeaders().add(Headers.TRANSFER_ENCODING, Headers.IDENTITY.toString());
+            exchange.getResponseHeaders().add(Headers.CONNECTION, Headers.KEEP_ALIVE.toString());
+            final StreamSinkChannel channel = exchange.getResponseChannel();
 
-            // Request headers for S3.
+            BoundRequestBuilder boundRequestBuilder = httpClient.prepareGet(S3.getS3Uri() + "/jay/README.md");
             for(String key: awsS3Request.getHeaders().keySet()) {
-                logger.info(key+"=>"+awsS3Request.getHeaders().get(key));
+                //logger.info(key+"=>"+awsS3Request.getHeaders().get(key));
                 boundRequestBuilder.addHeader(key, awsS3Request.getHeaders().get(key));
             }
 
-            // Async http client.
             boundRequestBuilder.execute(new AsyncCompletionHandler<Response>(){
+                @Override
+                public final State onHeadersReceived(final HttpHeaders headers) throws Exception {
+                    String contentLength = headers.get(Headers.CONTENT_LENGTH_STRING);
+                    if (contentLength != null) {
+                        exchange.getResponseHeaders().remove(Headers.TRANSFER_ENCODING);
+                        exchange.getResponseHeaders().remove(Headers.CONNECTION);
+                        exchange.getResponseHeaders().add(Headers.CONNECTION, Headers.KEEP_ALIVE.toString());
+                        exchange.setResponseContentLength(Long.parseLong(contentLength));
+                    }
+                    return super.onHeadersReceived(headers);
+                }
 
                 @Override
-                public void onThrowable(Throwable t){
+                public final State onBodyPartReceived(final HttpResponseBodyPart content) throws Exception {
+                    channel.write(content.getBodyByteBuffer());
+
+                    return State.CONTINUE;
+                }
+
+                @Override
+                public final void onThrowable(final Throwable t){
                     // Something wrong happened.
                     logger.error(t.getMessage());
                 }
 
                 @Override
-                public Response onCompleted(org.asynchttpclient.Response response) throws Exception {
-                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
-                    exchange.getResponseSender().send(Rocker.template(
-                            "kikaha/app/views/async.rocker.html", response.getResponseBody(Charset.defaultCharset()))
-                            .render().toString()
-                    );
+                public final Response onCompleted(final org.asynchttpclient.Response response) throws Exception {
+
+                    channel.shutdownWrites();
+                    channel.flush();
+                    channel.close();
 
                     return null;
                 }

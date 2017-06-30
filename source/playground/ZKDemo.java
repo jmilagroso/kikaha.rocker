83a956fa8a7a5aa6870708deddc7381abb7d287d
Jay Milagroso <j.milagroso@gmail.com>
2017-06-30 11:51:47 +0800

    Implements Apache Curator as a Zookeeper client library.

--- source/playground/ZKDemo.java
+++ source/playground/ZKDemo.java
@@ -1,16 +1,18 @@
 package playground;
 
 import com.relops.snowflake.Snowflake;
-import kikaha.app.zookeeper.ZKClientManagerImpl;
-import org.apache.zookeeper.KeeperException;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.test.TestingServer;
 import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 
 /**
@@ -23,86 +25,60 @@ public class ZKDemo {
     // MySQL Cluster Config Separator
     private static final char MySQLClusterConfigSeparator = '!';
 
+    private static final String     PATH = "/example/cache";
+
+    private static TestingServer server = null;
+    private static CuratorFramework    client = null;
+    private static PathChildrenCache cache = null;
+    private static Stat stat = null;
+
     public static void main(String args[]) {
 
-        // Zookeeper client manager impl.
-        ZKClientManagerImpl zkClientManager = new ZKClientManagerImpl();
-
-        // Zookeeper node path.
-        String path = "/MySQLClusters";
-
-        // "MySQL Cluster" config string array.
-        // TODO Fetch this from a .yml/.conf/.json over a type-safe config library.
-        String[] mysqlClusters = new String[]{
-                "host:192.168.0.1,user:app1,password:app1,port:3306",
-                "host:192.168.0.2,user:app2,password:app2,port:3306",
-                "host:192.168.0.3,user:app3,password:app3,port:3306",
-                "host:192.168.0.4,user:app4,password:app4,port:3306",
-                "host:192.168.0.5,user:app5,password:app5,port:3306",
-        };
-
-        // Byte array of the mysql cluster config data.
-        byte[] bytesArray = null;
-
         try {
-            // Byte array output stream container.
-            ByteArrayOutputStream output = new ByteArrayOutputStream();
-            for(String cluster : mysqlClusters) {
-                try {
-                    // Cluster separator.
-                    cluster += MySQLClusterConfigSeparator;
-                    // Write the each string cluster to byte
-                    // array output stream (byte[])
-                    output.write(cluster.getBytes());
-                } catch (IOException e) {
-                    logger.error(e.getLocalizedMessage());
-                }
-            }
-            // Output stream get byte array.
-            bytesArray = output.toByteArray();
-            // Remove the last pipe from the byte array.
-            bytesArray = Arrays.copyOfRange(bytesArray, 0, bytesArray.length - 1);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
 
-        // Arbitrary parameter.
-        // Should we update the Zookeeper values?
-        // TODO Handle the update invocation.
-        boolean update = false;
+            client = CuratorFrameworkFactory.newClient("localhost:2181",
+                    new ExponentialBackoffRetry(
+                            1000,
+                            3));
+            client.start();
 
-        try {
-            Stat stat = zkClientManager.getZNodeStats(path);
+            // Let's make sure we're connected to zookeeper first.
+            client.blockUntilConnected();
+
+            cache = new PathChildrenCache(client, PATH, true);
+            cache.start();
+
+            // Get metadata info
+            stat = client.getZookeeperClient().getZooKeeper().exists(PATH, true);
+
+            // Arbitrary parameter, in case we need to update the cluster config values.
+            // TODO Handle the update invocation.
+            boolean update = false;
+
+            // Data from zk node as cached.
+            String result = null;
+
             if (stat != null) {
-                // What if we need to update the data?
                 if(update) {
-                    zkClientManager.update(path, bytesArray);
+                    int version = client.getZookeeperClient().getZooKeeper().exists(PATH, true).getVersion();
+                    client.getZookeeperClient().getZooKeeper().setData(PATH, dummyData(), version);
                 }
-                // Access node path data.
-                String nodeData = (String)zkClientManager.getZNodeData(path,false);
-                logger.info("MySQL Cluster Config(s) from ZK = "+nodeData);
-
-                mimicDBSharding(nodeData);
+                result = new String(get(), "UTF-8");
             } else {
-                // Node path doesn't exist, let's create it.
-                zkClientManager.create(path, bytesArray);
+                // No node exists, create first then proceed with saving..
+                createParents(dummyData());
+                result = new String(get(), "UTF-8");
             }
-        } catch (KeeperException e) {
+
+            // Let's shard
+            shard(result);
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            zkClientManager.closeConnection();
         }
     }
 
-    private final static void mimicDBSharding(String mysqlDBCluster) {
-        logger.info("mysqlDBCluster = "+mysqlDBCluster);
-        logger.info(String.valueOf(mysqlDBCluster.split(String.valueOf(MySQLClusterConfigSeparator)).length));
-
-        String[] clusters = mysqlDBCluster.split(String.valueOf(MySQLClusterConfigSeparator));
-
-        List<Long> randomIds = new LinkedList<>();
+    private static void shard(String data) {
+        String[] clusters = data.split(String.valueOf(MySQLClusterConfigSeparator));
 
         // Dummy record to be inserted on the "database"
         int recordCount = 4096;
@@ -112,10 +88,7 @@ public class ZKDemo {
             // The node id is a manually assigned value between 0 and 1023 which is used to
             // differentiate different snowflakes when used in a multi-node cluster.
             long id = s.next();
-            randomIds.add(id);
-        }
 
-        for(Long id: randomIds) {
             int shard = (int)(id % clusters.length);
 
             // Each 'Shard' (1 Machine) has N sharded dbs.
@@ -126,4 +99,55 @@ public class ZKDemo {
             System.out.println("UUID:"+id + " | DB:mysql-host-"+clusters[shard] + " | db:" + db);
         }
     }
+
+    private static byte[] dummyData() {
+        // Byte array of the mysql cluster config data.
+        byte[] bytesArray = null;
+        // Prepare data to be saved/updated
+        String[] mysqlClusters = new String[]{
+                "host:192.168.0.1,user:app1,password:app1,port:3306",
+                "host:192.168.0.2,user:app2,password:app2,port:3306",
+                "host:192.168.0.3,user:app3,password:app3,port:3306",
+                "host:192.168.0.4,user:app4,password:app4,port:3306",
+                "host:192.168.0.5,user:app5,password:app5,port:3306",
+        };
+        // Byte array output stream container.
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        for(String cluster : mysqlClusters) {
+            try {
+                // Cluster separator.
+                cluster += MySQLClusterConfigSeparator;
+                // Write the each string cluster to byte
+                // array output stream (byte[])
+                output.write(cluster.getBytes());
+            } catch (IOException e) {
+                logger.error(e.getLocalizedMessage());
+            }
+        }
+        // Output stream get byte array.
+        bytesArray = output.toByteArray();
+        // Remove the last pipe from the byte array.
+        bytesArray = Arrays.copyOfRange(bytesArray, 0, bytesArray.length - 1);
+
+        return bytesArray;
+    }
+
+    private static byte[] get() {
+        try {
+            return client.getZookeeperClient().getZooKeeper().getData(PATH, null,null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    private static void createParents(byte[] bytesArray) {
+        try {
+            client.create().creatingParentsIfNeeded().forPath(PATH, bytesArray);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
